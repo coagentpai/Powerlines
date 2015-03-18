93d6549f49f6f6f4d9a35571692c77992883a9e3
Peter Irish <coagentpai@gmail.com>
2015-03-17 22:34:36 -0500

    commands: Add command names for the known commands
    The build should fail since some commands are missing from CommandAliases. (testing CI!)

--- powerlines/commands.go
+++ powerlines/commands.go
@@ -6,17 +6,71 @@ const (
 	Hello Command = iota
 	Goodbye
 	WorldInfo
+	PlayerJoin
 	AllPlayers
 	AllPlayersPosition
 	SendMsg
 	RecieveMsg
 )
+var AllCommands = []Command{
+	Hello,
+	Goodbye,
+	WorldInfo,
+	PlayerJoin,
+	AllPlayers,
+	AllPlayersPosition,
+	SendMsg,
+	RecieveMsg,
+}
 
-var CommandReadable = map[Command]string{
-	Hello: "Say hello to a server",
-    Goodbye: "ahskjahsdj",
-	AllPlayers: "sakljsdal",
-	AllPlayersPosition: "klsjsshkjsa",
-	WorldInfo: "sdakllsdaj",
+type CommandAlias struct {
+	command Command
+	short string
+	long string
+}
+
+var CommandAliases = map[Command]CommandAlias {
+	Hello: CommandAlias{
+	long:
+		"Say hello to a server",
+	short:
+		"Hello",
+	},
+    Goodbye: CommandAlias{
+	long:
+		"Say goodbye to the server",
+	short:
+		"Goodbye",
+	},
+	PlayerJoin: CommandAlias{
+	long:
+		"Join the world",
+	short:
+		"PlayerJoin",
+	},
+	AllPlayers: CommandAlias{
+	long:
+		"List all connected players",
+	short:
+		"AllPlayers",
+	},
+	AllPlayersPosition: CommandAlias{
+	long:
+		"List the position of all players",
+	short:
+		"AllPlayersPosition",
+	},
+	WorldInfo: CommandAlias{
+	long:
+		"Return metadata about the current world being played",
+	short:
+		"WorldInfo",
+	},
+}
+
+func init() {
+	for command, alias := range CommandAliases {
+		alias.command = command
+	}
 }
 
